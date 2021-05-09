62ca90a4b71f1a7ff9f06b7f926353a944618220
Ravi Zadpide <ravi.zadpide@scoperetail.com>
2021-05-09 19:14:03 +0530

    Single queue multiple listeners.

--- src/main/java/com/scoperetail/fusion/messaging/adapter/in/messaging/jms/MessageReceiver.java
+++ src/main/java/com/scoperetail/fusion/messaging/adapter/in/messaging/jms/MessageReceiver.java
@@ -3,6 +3,9 @@ package com.scoperetail.fusion.messaging.adapter.in.messaging.jms;
 
 import static com.scoperetail.fusion.messaging.adapter.in.messaging.jms.TaskResult.DISCARD;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
@@ -10,16 +13,20 @@ import javax.jms.Session;
 import org.springframework.jms.listener.SessionAwareMessageListener;
 import org.springframework.jms.support.converter.SimpleMessageConverter;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@AllArgsConstructor
 @Slf4j
 public class MessageReceiver implements SessionAwareMessageListener<Message> {
 
-	private String queue;
-	private String brokerId;
-	private MessageListener<String> messageListener;
+	private final String queue;
+	private final String brokerId;
+	private final List<MessageListener<String>> messageListeners = new ArrayList<>(1);
+
+	public MessageReceiver(final String queue, final String brokerId, final MessageListener<String> messageListener) {
+		this.queue = queue;
+		this.brokerId = brokerId;
+		registerListener(messageListener);
+	}
 
 	@Override
 	public void onMessage(final Message message, final Session session) throws JMSException {
@@ -27,13 +34,29 @@ public class MessageReceiver implements SessionAwareMessageListener<Message> {
 		final SimpleMessageConverter smc = new SimpleMessageConverter();
 		final String strMessage = String.valueOf(smc.fromMessage(message));
 		TaskResult taskResult = DISCARD;
-		try {
-			taskResult = messageListener.doTask(strMessage);
-		} catch (final Throwable e) {
-			log.error("=================ERROR MESSAGE DUMP START=========================");
-			log.error("Unable to handle message: {} due to exception:{}", message, e);
-			log.error("=================ERROR MESSAGE DUMP END=========================");
+		int noOfListenersHandledMessage = 0;
+		for (final MessageListener<String> messageListener : messageListeners) {
+			try {
+				if (messageListener.canHandle(strMessage)) {
+					noOfListenersHandledMessage++;
+					taskResult = messageListener.doTask(strMessage);
+					log.info("Message handling status is: {}", taskResult);
+				}
+			} catch (final Throwable e) {
+				log.error("=================ERROR MESSAGE DUMP START=========================");
+				log.error("Unable to handle error message: {} due to exception:{}", message, e);
+				log.error("=================ERROR MESSAGE DUMP END=========================");
+			}
 		}
-		log.info("Message handling status is: {}", taskResult);
+		if (noOfListenersHandledMessage == 0) {
+			log.error("None of the listeners handled the message");
+			log.error("=================INVALID MESSAGE DUMP START=========================");
+			log.error("Unable to handle invalid message: {} ", message);
+			log.error("=================INVALID MESSAGE DUMP END=========================");
+		}
+	}
+
+	public void registerListener(final MessageListener<String> messageListener) {
+		messageListeners.add(messageListener);
 	}
 }
--- src/main/java/com/scoperetail/fusion/messaging/adapter/out/messaging/jms/impl/MessageRouterReceiverService.java
+++ src/main/java/com/scoperetail/fusion/messaging/adapter/out/messaging/jms/impl/MessageRouterReceiverService.java
@@ -1,6 +1,9 @@
 /* ScopeRetail (C)2021 */
 package com.scoperetail.fusion.messaging.adapter.out.messaging.jms.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jms.Queue;
 
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
@@ -12,6 +15,7 @@ import com.scoperetail.fusion.messaging.adapter.in.messaging.jms.RouterHelper;
 import com.scoperetail.fusion.messaging.adapter.out.messaging.jms.MessageRouterReceiver;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -19,22 +23,38 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MessageRouterReceiverService implements MessageRouterReceiver {
 
-	RouterHelper routerHelper;
+	private RouterHelper routerHelper;
+	private final Map<Endpoint, MessageReceiver> dmlcByBrokerAndQueueMap = new HashMap<>(1);
 
 	@Override
 	public void registerListener(final String brokerId, final String queueName,
 			final MessageListener<String> messageListener) {
-		final DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
-		defaultMessageListenerContainer.setConnectionFactory(routerHelper.getConnectionFactory(brokerId));
-		defaultMessageListenerContainer.setMessageListener(new MessageReceiver(queueName, brokerId, messageListener));
-		final Queue queue = routerHelper.getQueue(queueName);
-		defaultMessageListenerContainer.setDestination(queue);
-		defaultMessageListenerContainer.setCacheLevelName("CACHE_CONSUMER");
-		defaultMessageListenerContainer.setConcurrency("5-10");
-		defaultMessageListenerContainer.setAutoStartup(true);
-		defaultMessageListenerContainer.setSessionTransacted(false);
-		defaultMessageListenerContainer.afterPropertiesSet();
-		defaultMessageListenerContainer.start();
+		final Endpoint endpoint = new Endpoint(brokerId, queueName);
+		if (!dmlcByBrokerAndQueueMap.containsKey(endpoint)) {
+			final DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
+			defaultMessageListenerContainer.setConnectionFactory(routerHelper.getConnectionFactory(brokerId));
+			final MessageReceiver messageReceiver = new MessageReceiver(queueName, brokerId, messageListener);
+			defaultMessageListenerContainer.setMessageListener(messageReceiver);
+			final Queue queue = routerHelper.getQueue(queueName);
+			defaultMessageListenerContainer.setDestination(queue);
+			defaultMessageListenerContainer.setCacheLevelName("CACHE_CONSUMER");
+			defaultMessageListenerContainer.setConcurrency("5-10");
+			defaultMessageListenerContainer.setAutoStartup(true);
+			defaultMessageListenerContainer.setSessionTransacted(false);
+			defaultMessageListenerContainer.afterPropertiesSet();
+			defaultMessageListenerContainer.start();
+			dmlcByBrokerAndQueueMap.put(endpoint, messageReceiver);
+		} else {
+			final MessageReceiver messageReceiver = dmlcByBrokerAndQueueMap.get(endpoint);
+			messageReceiver.registerListener(messageListener);
+		}
 		log.info("Initialized listener on queue: {} for brokerId:{}", queueName, brokerId);
+	}
+
+	@Data
+	@AllArgsConstructor
+	class Endpoint {
+		String brokerId;
+		String queueName;
 	}
 }
